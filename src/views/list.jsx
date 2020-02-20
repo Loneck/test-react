effaef97a0cf5acb57d34227ea1bcebd8734f749
ltorrejon <leo.torrejon@gmail.com>
2020-02-20 11:42:10 -0300

    fix car list

--- src/views/list.jsx
+++ src/views/list.jsx
@@ -3,52 +3,68 @@ import DataService from '../services/list';
 
 function List(props) {
   const [collection, setCollection] = useState([])
+  const { type } = props.match.params;
 
   useEffect(() => {
     async function fetchData() {
-      const { type } = props.match.params;
       let _source = null;
       console.log(type);
       switch (type) {
         case 'motos':
           _source = DataService.getBikes.then(r => r);
+          _source
+              .then(r => r.json())
+              .then(
+                  body => setCollection([...body.data])
+              )
           break;
         case 'cars':
           _source = DataService.getCars.then(r => r);
+          _source
+              .then(r => r.json())
+              .then(
+                  body => setCollection([...body])
+              )
           break;
         default: break;
       }
-      _source
-          .then(r => r.json()
-              .then(
-                  body => setCollection({...body, ...body.data})
-              )
-          )
     };
     fetchData();
-    console.log(collection.length);
   }, []);
-
-  console.log(collection.length);
 
   return (
       <div className="container">
         <div className="row">
-          {
+          {type === "motos" ? (
             collection.length > 0 && collection.map(
+              o => (
+                <div className="col-4">
+                  <div className="card mb-3">
+                    <div className="card-body">
+                      <div><img src="https://via.placeholder.com/300.png/09f/fff" alt="" style={{ borderRadius: '20px' }} /></div>
+                      <div style={{ padding: '5px' }}>id: {o.id}</div>
+                      <div style={{ padding: '5px' }}>modelo: {o.name}</div>
+                    </div>
+                  </div>
+                </div>
+              )
+            )
+          ) : (collection.length > 0 && collection.map(
                 o => (
-                    <div className="col-4">
-                      <div className="card mb-3">
-                        <div className="card-body">
-                          <div><img src="https://via.placeholder.com/300.png/09f/fff" alt="" style={{ borderRadius: '20px' }} /></div>
-                          <div style={{ padding: '5px' }}>id: {o.id}</div>
-                          <div style={{ padding: '5px' }}>modelo: {o.name}</div>
-                        </div>
+                  <div className="col-4">
+                    <div className="card mb-3">
+                      <div className="card-body">
+                        <div><img src="https://via.placeholder.com/300.png/09f/fff" alt="" style={{ borderRadius: '20px' }} /></div>
+                        <div style={{ padding: '5px' }}>modelo: {o.Name}</div>
+                        <div style={{ padding: '5px' }}>año: {o.Year}</div>
+                        <div style={{ padding: '5px' }}>origen: {o.Origin}</div>
                       </div>
                     </div>
+                  </div>
                 )
-            )
-          }
+              )
+
+          )}
         </div>
       </div>
   )
